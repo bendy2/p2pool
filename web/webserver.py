9f09b8ba20c7a48e9e6489c463d75f8e236e94e5
Bedy <67052@qq.com>
2025-05-14 22:58:09 +0800

    11

--- web/webserver.py
+++ web/webserver.py
@@ -47,7 +47,7 @@ def get_db_connection():
 
 def read_stratum_data():
     try:
-        with open('.api/local/stratum', 'r') as f:
+        with open('./api/local/stratum', 'r') as f:
             data = json.load(f)
             return {
                 'hashrate_15m': data.get('hashrate_15m', 0),
