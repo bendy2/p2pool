e4c75c10c03bb8d63e031612cbaa5bbdec5f33f1
Bedy <67052@qq.com>
2025-05-20 04:18:40 +0800

    22

--- web/web.py
+++ web/web.py
@@ -289,4 +289,4 @@ async def get_hashrate_history():
 # 启动服务器
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run(app, host="0.0.0.0", port=80) 
